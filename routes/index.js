283905d8ca64afa4cd030e939103615f657844a2
Amodoi Peter <amodoipeter@gmail.com>
2023-03-08 06:38:43 +0300

    First API

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,10 @@
 import { Router } from 'express';
-import { AppController } from '../controllers/AppController';
-import { UsersController} from '../controllers/UsersController';
-import { AuthController } from '../controllers/AuthController';
-import { FileController } from '../controllers/FileController';
+import AppController from '../controllers/AppController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 
-router.get('/stats', StatsController.getStats);
+router.get('/stats', AppController.getStats);
 
-router.get('/users', UsersController.postNew)
-
-router.get('/connect', AuthController.getConnect);
-
-router.get('/disconnect', AuthController.getDisconnect);
-
-router.get('/users/me', UsersController.getMe);
-
-router.post('/files', FilesController.postUpload);
-
-router.get('/files/:id', FilesController.getShow);
-
-router.get('/files', FilesController.getIndex);
-
-router.get('/files/:id/publish', FilesController.putPublish);
-
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-router.get('/files/:id/data', FilesController.getFile);
-
-module.exports 
+module.exports = router;
